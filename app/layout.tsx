add5676c1ff7ccfe5084faae741a3b6e92d6aa6e
nam-dohai <focman26@gmail.com>
2023-08-18 09:24:24 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css';
-
-import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
-import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
